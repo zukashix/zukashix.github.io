59b1200e5ae024b01ffb5cb9351cacc3362f1894
Divyanshu Shekhar <67629199+zukashix@users.noreply.github.com>
2023-12-12 13:07:21 +0530

    Delete schoolip directory

--- schoolip/table_print.py
+++
@@ -1,8 +0,0 @@
-# Program to print tables of any input number
-
-# Take input
-num = float(input("Which number's table do you want to know?: "))
-times = int(input("How many table multiplications do you want to print?: "))
-
-for i in range(times):
-    print(f'{num} x {i + 1} = {num * (i + 1)}')
